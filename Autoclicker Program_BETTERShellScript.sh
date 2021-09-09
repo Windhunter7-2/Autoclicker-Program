5560de2b190cea89e700766d2281cec7a19cc0cb
ebaker23 <ebaker23@gmu.edu>
2021-09-09 10:36:26 -0400

    Updated (Bettered) ShellScript

--- Autoclicker Program_BETTERShellScript.sh
+++ Autoclicker Program_BETTERShellScript.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#Put Your Installation Directory for Windhunter's Programs HERE:
-programDirectory="Z:/Windhunter's Programs"
+#Get Txt File Contents
+txtFolder="C:/Users/Public/"
+txtFile="Windhunter's Programs - Main Folder Location.txt"
+programDirectory=$(cat $txtFolder"$txtFile")
 
-#Below Is the Code to Load the Program; You Shouldn't Need to Worry About It...
+#Load the Program
 directoryA="$programDirectory/Settings/JavaFX_SDK_16/lib"
 directoryB="$programDirectory/Autoclicker-Program/Autoclicker Program.jar"
 java --module-path "$directoryA" --add-modules javafx.controls,javafx.fxml -jar "$directoryB"
